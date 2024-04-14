ace8685e8083002b5865c730a9d73b558e4eff69
Dakota <dakotawkeast@gmail.com>
2024-04-13 21:48:17 -0400

    worked styling and caught bug to work on

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+//button stling needs work: width at regular screen size seems small and on mobile sizes, interferes with text in description box
 export const UserInput = ({ doneTask, handleSubmit, handleChange }) => {
   return (
     <form onSubmit={handleSubmit}>
         <input 
         name='title' 
         value={doneTask.title || ''} 
-        placeholder="What have you accomplished?" 
+        placeholder="What have you done?" 
         onChange={handleChange} 
         />
         {!doneTask.title ? null : (
